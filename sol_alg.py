f8549c72d6ceafb093ef3c34d43a63cee199e6a9
renimatsa <shan@Shan.local>
2017-05-09 13:54:53 +0800

    Search Insert Position
    Algorithm

--- sol_alg.py
+++ sol_alg.py
@@ -3,3 +3,6 @@ def isPalindrome(x):
 	y = [ch for ch in str(x)]
 	return list(reversed(y)) == y
 
+# Return the index if the target exists. If not, return the index where it would be.
+def searchInsert(nums, target):
+	return nums.index(target) if target in nums else len(filter(lambda x: x < target, nums))
